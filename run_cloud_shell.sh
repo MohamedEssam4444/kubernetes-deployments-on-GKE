a08ff8f73fa7c7e55e3c8a4d3475b51d54daccc8
MohamedEssam4444 <68178003+MohamedEssam4444@users.noreply.github.com>
2020-11-29 13:04:24 +0200

    Update run_cloud_shell.sh

--- run_cloud_shell.sh
+++ run_cloud_shell.sh
@@ -5,9 +5,11 @@ gcloud container clusters get-credentials $my_cluster --zone $my_zone
 kubectl apply -f ./nginx-deployment.yaml
 kubectl get deployments
 kubectl apply -f ./service-nginx.yaml
+#To update the version of nginx in the deployment, execute the following command
+kubectl set image deployment.v1.apps/nginx-deployment nginx=nginx:1.9.1 --record#This updates the container image in your Deployment to nginx v1.9.1.
 kubectl apply -f nginx-canary.yaml
 #When the deployment is complete, verify that both the nginx and the nginx-canary deployments are present.
 kubectl get deployments
-#scale down the primary deployment to 0 replicas
+#scale down the primary deployment to 0 replicas so that know after testing the canary deployment on fewer number of users and finding it worked fine will make all trrafic goes to the newer pod with newest image version .
 kubectl scale --replicas=0 deployment nginx-deployment
 kubectl get deployments
